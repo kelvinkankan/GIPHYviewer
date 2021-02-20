943ab6b2d34a9e4db4829290a39fe86ffb9d8c5d
kelvinkankan <43528189+kelvinkankan@users.noreply.github.com>
2021-02-20 19:00:50 +0800

    Initial Commit

---
+++ GIPHYviewer/GIPHYviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIPHYviewerApp.swift
+//  GIPHYviewer
+//
+//  Created by Kevin Kan on 20/2/2021.
+//
+
+import SwiftUI
+
+@main
+struct GIPHYviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
